042426e9d316efce34dcef4f79477a1d38280207
Joe <Walktheworld@me.com>
2022-02-10 13:47:39 -0700

    commit tree 497f160f7c9dd2e276a7c6e4767955d25637d88a

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,67 @@
 # your code goes here
+require "pry"
+
+class Person
+    attr_reader :name
+    attr_accessor :bank_account, :happiness, :hygiene
+
+    def initialize(name)
+        @name= name
+        @bank_account= 25
+        @happiness= 8
+        @hygiene= 8
+
+    end
+    
+    def happiness=(num)
+        @happiness= if num > 10
+                        10
+                    elsif num < 0
+                        0
+                    else
+                     num
+                    end
+    end
+
+    def hygiene=(num)
+        @hygiene=   if num > 10
+                        10
+                    elsif num < 0
+                         0
+                     else
+                         num
+                    end
+    end
+
+    def happy?
+        if self.happiness >7
+            true
+        else false
+        end
+    end
+
+    def clean?
+        if self.hygiene >7
+            true
+        else false
+        end
+    end
+
+    def get_paid(num)
+        self.bank_account += num
+        return "all about the benjamins"
+    end
+
+    def take_bath
+        self.hygiene += 4
+        return "♪ Rub-a-dub just relaxing in the tub ♫"
+    end
+
+    def work_out
+        self.hygiene -= 3
+        self.happiness += 2
+        return "♪ another one bites the dust ♫"
+    end
+
+
+end
